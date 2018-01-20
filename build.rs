994bec8706c9cef183d585300eeaf54da9fad23c
manpat <pat@patsown.com>
2018-01-20 04:11:08 +0000

    Updated html

--- build.rs
+++ build.rs
@@ -6,13 +6,14 @@ use std::path::Path;
 const INDEX_HTML_TEMPLATE: &'static str = 
 r##"<html>
 	<head>
+		<meta charset="utf-8" />
 		<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no' />
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="mobile-web-app-capable" content="yes" />
 
-		<meta name="theme-color" content="#222">
-		<meta name="msapplication-navbutton-color" content="#222">
-		<meta name="apple-mobile-web-app-status-bar-style" content="#222">
+		<meta name="theme-color" content="#222" />
+		<meta name="msapplication-navbutton-color" content="#222" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="#222" />
 
 		<style>
 			* {
