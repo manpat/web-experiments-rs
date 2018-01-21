7639c545acc1346ed238c2beae2d853b7206e9c2
manpat <pat@patsown.com>
2018-01-21 14:02:42 +0000

    Removed inline js from resize event - using css to resize element; Made touch handlers consume events

--- build.rs
+++ build.rs
@@ -33,6 +33,12 @@ r##"<html>
 			}
 
 			canvas {
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
+
 				overflow: hidden;
 				display: block;
 			}
--- src/events.rs
+++ src/events.rs
@@ -18,37 +18,47 @@ pub enum Event {
 pub unsafe fn initialise_ems_event_queue(queue: &mut Vec<Event>) {
 	use std::ptr::null;
 
+	js! { b"Module.canvas = document.getElementById('canvas')\0" };
+
 	let evt_ptr = transmute(queue);
 
-	let target = b"#document\0".as_ptr() as _;
+	let window_target = b"#window\0".as_ptr() as _;
+	let canvas_target = b"#canvas\0".as_ptr() as _;
 
 	on_resize(0, null(), evt_ptr);
-	emscripten_set_resize_callback(null(), evt_ptr, 0, Some(on_resize));
-	emscripten_set_click_callback(target, evt_ptr, 1, Some(on_click));
+	emscripten_set_resize_callback(window_target, evt_ptr, 1, Some(on_resize));
+	emscripten_set_click_callback(canvas_target, evt_ptr, 1, Some(on_click));
 
-	emscripten_set_mousemove_callback(target, evt_ptr, 0, Some(on_mouse_move));
-	emscripten_set_mousedown_callback(target, evt_ptr, 0, Some(on_mouse_down));
-	emscripten_set_mouseup_callback(target, evt_ptr, 0, Some(on_mouse_up));
+	emscripten_set_mousemove_callback(canvas_target, evt_ptr, 1, Some(on_mouse_move));
+	emscripten_set_mousedown_callback(canvas_target, evt_ptr, 1, Some(on_mouse_down));
+	emscripten_set_mouseup_callback(canvas_target, evt_ptr, 1, Some(on_mouse_up));
 
-	emscripten_set_touchstart_callback(target, evt_ptr, 1, Some(on_touch_start));
-	emscripten_set_touchmove_callback(target, evt_ptr, 1, Some(on_touch_move));
-	emscripten_set_touchend_callback(target, evt_ptr, 1, Some(on_touch_end));
-	emscripten_set_touchcancel_callback(target, evt_ptr, 1, Some(on_touch_end));
+	emscripten_set_touchstart_callback(canvas_target, evt_ptr, 1, Some(on_touch_start));
+	emscripten_set_touchmove_callback(canvas_target, evt_ptr, 1, Some(on_touch_move));
+	emscripten_set_touchend_callback(canvas_target, evt_ptr, 1, Some(on_touch_end));
+	emscripten_set_touchcancel_callback(canvas_target, evt_ptr, 1, Some(on_touch_end));
 }
 
 unsafe extern "C"
 fn on_resize(_: i32, _e: *const EmscriptenUiEvent, ud: *mut CVoid) -> i32 {
 	let event_queue: &mut Vec<Event> = transmute(ud);
 
-	js! { b"Module.canvas = document.getElementById('canvas')\0" };
+	// js! { b"Module.canvas = document.getElementById('canvas')\0" };
 
-	let mut screen_size = Vec2i::zero();
-	screen_size.x = js! { b"return (Module.canvas.width = Module.canvas.style.width = window.innerWidth)\0" };
-	screen_size.y = js! { b"return (Module.canvas.height = Module.canvas.style.height = window.innerHeight)\0" };
+	// let mut screen_size = Vec2i::zero();
+	// screen_size.x = js! { b"return (Module.canvas.width = Module.canvas.style.width = window.innerWidth)\0" };
+	// screen_size.y = js! { b"return (Module.canvas.height = Module.canvas.style.height = window.innerHeight)\0" };
+	// event_queue.push(Event::Resize(screen_size));
 
-	event_queue.push(Event::Resize(screen_size));
+	let canvas_target = b"#canvas\0".as_ptr() as _;
+	let (mut width, mut height) = (0.0, 0.0);
+
+	emscripten_get_element_css_size(canvas_target, &mut width, &mut height);
+	emscripten_set_canvas_size(width as i32, height as i32);
+
+	event_queue.push(Event::Resize(Vec2i::new(width as i32, height as i32)));
 	
-	0
+	1
 }
 
 unsafe extern "C"
@@ -101,7 +111,7 @@ fn on_touch_move(_: i32, e: *const EmscriptenTouchEvent, ud: *mut CVoid) -> i32 
 	let pos = Vec2i::new(e.touches[0].clientX as _, e.touches[0].clientY as _);
 	event_queue.push(Event::Move(pos));
 	
-	0
+	1
 }
 
 unsafe extern "C"
@@ -114,7 +124,7 @@ fn on_touch_start(_: i32, e: *const EmscriptenTouchEvent, ud: *mut CVoid) -> i32
 	let pos = Vec2i::new(e.touches[0].clientX as _, e.touches[0].clientY as _);
 	event_queue.push(Event::Down(pos));
 	
-	0
+	1
 }
 
 unsafe extern "C"
@@ -122,20 +132,22 @@ fn on_touch_end(_: i32, e: *const EmscriptenTouchEvent, ud: *mut CVoid) -> i32 {
 	let event_queue: &mut Vec<Event> = transmute(ud);
 	let e: &EmscriptenTouchEvent = transmute(e);
 
-	if e.touches[0].identifier != 0 { return 0 }
+	if e.numTouches > 1 {
+		use std::mem::uninitialized;
 
-	use std::mem::uninitialized;
+		// TODO: Make this requestable
+		let mut fse: EmscriptenFullscreenChangeEvent = uninitialized();
+		emscripten_get_fullscreen_status(&mut fse);
 
-	// TODO: Make this requestable
-	let mut fse: EmscriptenFullscreenChangeEvent = uninitialized();
-	emscripten_get_fullscreen_status(&mut fse);
-
-	if fse.isFullscreen == 0 {
-		js!{ b"Module.requestFullscreen(1,1,0)" };
+		if fse.isFullscreen == 0 {
+			js!{ b"Module.requestFullscreen(1,1,0)" };
+		}
 	}
+
+	if e.touches[0].identifier != 0 { return 0 }
 
 	let pos = Vec2i::new(e.touches[0].clientX as _, e.touches[0].clientY as _);
 	event_queue.push(Event::Up(pos));
 	
-	0
+	1
 }
