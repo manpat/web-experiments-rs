d79fc059541de59884de14b00dcf3e6e4cf7c124
manpat <pat@patsown.com>
2018-02-22 22:54:10 +0000

    Simplifications; Started looking into basic ui

--- src/bin/toroidal-tiles.rs
+++ src/bin/toroidal-tiles.rs
@@ -19,6 +19,9 @@ impl Vertex for Vert2D {
 	}
 }
 
+const TEXELS_PER_TILE: u32 = 16;
+const TEXEL_FACTOR: Vec2 = Vec2::splat(1.0 / 128.0);
+
 fn main() {
 	std::env::set_var("RUST_BACKTRACE", "1");
 
@@ -51,32 +54,77 @@ fn main() {
 		let tex = Texture::from_png(bin_asset!("tileset.png"));
 
 		let tile_set = {
+			use tile_flags::*;
+
 			let texel_size = Vec2i::splat(16);
 			TileSet {
 				tile_infos: vec![
 					TileInfo {
-						name: "",
+						name: "ground",
 						texel_offset: Vec2i::zero(),
-						texel_size
+						texel_size,
+						flags: 0,
 					},
 
 					TileInfo {
-						name: "",
+						name: "ladder",
 						texel_offset: Vec2i::new(16, 0),
-						texel_size
+						texel_size,
+						flags: ALLOWS_Z_MOVE,
 					},
 
 					TileInfo {
 						name: "player_idle",
 						texel_offset: Vec2i::new(32, 0),
-						texel_size
-					}
+						texel_size,
+						flags: 0,
+					},
+
+					TileInfo {
+						name: "cursor",
+						texel_offset: Vec2i::new(48, 0),
+						texel_size,
+						flags: 0,
+					},
+
+					TileInfo {
+						name: "generator",
+						texel_offset: Vec2i::new(32, 0),
+						texel_size,
+						flags: 0,
+					},
+
+					TileInfo {
+						name: "storage",
+						texel_offset: Vec2i::new(32, 0),
+						texel_size,
+						flags: 0,
+					},
+
+					TileInfo {
+						name: "pipe_base",
+						texel_offset: Vec2i::new(32, 0),
+						texel_size,
+						flags: 0,
+					},
+					TileInfo {
+						name: "pipe_connector",
+						texel_offset: Vec2i::new(32, 0),
+						texel_size,
+						flags: 0,
+					},
 				]
 			}
 		};
 
 		let mut world_view = WorldView::new(&tex);
 		let mut world = World::new(tile_set);
+
+		let mut show_menu = false;
+		let mut mouse_pos = Vec2i::zero();
+
+		let mut ui_mesh = Mesh::new();
+		let mut ui_builder = MeshBuilder::new();
 
 		loop {
 			for e in events.iter() {
@@ -99,6 +147,10 @@ fn main() {
 						}
 					}
 
+					Event::Move(pos) => {
+						mouse_pos = pos; 
+					}
+
 					Event::KeyDown(k) => {
 						let prev_pos = world.player_pos;
 
@@ -111,9 +163,29 @@ fn main() {
 							KeyCode::Alpha('D') => if world.move_player(Vec2i::new( 1, 0)) { did_warp = true }
 							KeyCode::Alpha('A') => if world.move_player(Vec2i::new(-1, 0)) { did_warp = true }
 	
-							KeyCode::Alpha('Q') => if world.shift_layer(-1) { did_shift = true }
-							KeyCode::Alpha('E') => if world.shift_layer( 1) { did_shift = true }
+							KeyCode::Alpha('Q') => {
+								let z_move_allowed = world.get_tile_info(prev_pos.to_vec2i())
+									.map(|ti| ti.allows_z_move())
+									.unwrap_or(false);
+
+								if z_move_allowed {
+									did_shift |= world.shift_layer(-1)
+								}
+							}
+
+							KeyCode::Alpha('E') => {
+								let z_move_allowed = world.get_tile_info(prev_pos.to_vec2i())
+									.map(|ti| ti.allows_z_move())
+									.unwrap_or(false);
+
+								if z_move_allowed {
+									did_shift |= world.shift_layer( 1)
+								}
+							}
 	
+							KeyCode::Tab => {
+								show_menu ^= true;
+							}
 							_ => {}
 						}
 
@@ -141,6 +213,20 @@ fn main() {
 			shader.set_uniform_i32("u_color", 0);
 
 			world_view.draw(&world);
+
+			{	let cursor = world.tile_set.get_tile_info_by_name("cursor").unwrap();
+
+				let Vec2{x, y} = world_view.transform_screen_coord_to_tile(mouse_pos);
+				let mouse_tile = Vec2::new(x.floor(), y.floor());
+
+				ui_builder.draw_tiled(cursor.texel_offset, cursor.texel_size, mouse_tile);
+	
+				ui_builder.upload_to(&mut ui_mesh);
+				ui_builder.clear();
+
+				ui_mesh.bind();
+				ui_mesh.draw(gl::TRIANGLES);
+			}
 
 			yield;
 		}
@@ -196,14 +282,14 @@ impl WorldView {
 			let player_info = world.tile_set.get_tile_info_by_name("player_idle")
 				.expect("missing player sprite");
 			
+			let tile_set_lookup = |idx| world.tile_set.get_tile_info(idx as usize);
+
 			for y in bottom.y..top.y {
 				for x in bottom.x..top.x {
 					let pos = Vec2i::new(x, y);
 
-					if let Some(tile_idx) = world.get_tile(pos) {
-						if let Some(info) = world.tile_set.get_tile_info(tile_idx as usize) {
-							self.draw_tile(info, pos);
-						}
+					if let Some(Some(info)) = world.get_tile(pos).map(&tile_set_lookup) {
+						self.draw_tile(info, pos);
 					}
 
 					if world.wrap_position(pos) == world.player_pos.to_vec2i() {
@@ -220,28 +306,32 @@ impl WorldView {
 		self.mesh.draw(gl::TRIANGLES);
 	}
 
+	fn draw_tile_rotated(&mut self, tile_info: &TileInfo, pos: Vec2i, rot: u32) {
+		self.builder.draw_tiled_rotated(tile_info.texel_offset, tile_info.texel_size, pos.to_vec2(), rot);
+	}
+
 	fn draw_tile(&mut self, tile_info: &TileInfo, pos: Vec2i) {
-		let texel_factor = Vec2::splat(1.0) / self.texture_size.to_vec2();
-
-		let uv = tile_info.texel_offset.to_vec2() * texel_factor;
-		let size = tile_info.texel_size.to_vec2() * texel_factor;
-
-		let pos = pos.to_vec2();
-
-		self.builder.add_quad(&[
-			Vert2D(Vec2::new(0.0, 0.0) + pos, uv + Vec2::new(0.01, 0.98) * size),
-			Vert2D(Vec2::new(0.0, 1.0) + pos, uv + Vec2::new(0.01, 0.01) * size),
-			Vert2D(Vec2::new(1.0, 1.0) + pos, uv + Vec2::new(0.98, 0.01) * size),
-			Vert2D(Vec2::new(1.0, 0.0) + pos, uv + Vec2::new(0.98, 0.98) * size),
-		]);
+		self.draw_tile_rotated(tile_info, pos, 0);
 	}
 }
 
+
+mod tile_flags {
+	pub const ALLOWS_Z_MOVE: u32 = 1<<0;
+	pub const BLOCKS_MOVE: u32 = 1<<1;
+}
 
 struct TileInfo {
 	name: &'static str,
 	texel_offset: Vec2i,
 	texel_size: Vec2i,
+	flags: u32,
+}
+
+impl TileInfo {
+	fn allows_z_move(&self) -> bool {
+		self.flags & tile_flags::ALLOWS_Z_MOVE > 0
+	}
 }
 
 struct TileSet {
@@ -252,10 +342,8 @@ impl TileSet {
 	fn get_tile_info(&self, index: usize) -> Option<&TileInfo> {
 		if index == 0 {
 			None
-		} else if index-1 < self.tile_infos.len() {
-			Some(&self.tile_infos[index-1])
 		} else {
-			None
+			self.tile_infos.get(index-1)
 		}
 	}
 
@@ -307,9 +395,17 @@ impl TileMap {
 			None
 		}
 	}
+
+	fn wrap_position(&self, pos: Vec2i) -> Vec2i {
+		Vec2i::new(
+			(pos.x%self.size.x + self.size.x) % self.size.x,
+			(pos.y%self.size.y + self.size.y) % self.size.y)
+	}
 }
 
 struct World {
+	// TODO: some way to chunk larger tilemaps
+	// Vec<Box<TileMapTrait>>?
 	layers: Vec<TileMap>,
 	tile_set: TileSet,
 
@@ -322,34 +418,36 @@ impl World {
 		let mut layers = Vec::new();
 
 		for i in 0..6 {
-			let world_size = Vec2i::splat(2i32.pow(i + 2));
+			let world_size = Vec2i::splat(World::layer_size(i));
 
-			let mut layer = TileMap::new(world_size);
-			layer.set_tiles_from(|v| if v.x.min(v.y) > 0 {1} else {2});
-			layers.push(layer);
+			layers.push(TileMap::new(world_size));
 		}
+
+		let player_layer = layers.len() as i32 - 1;
 
 		World {
 			layers,
 			tile_set,
 
 			player_pos: Vec2::new(2.5, 2.5),
-			player_layer: 2,
+			player_layer,
 		}
 	}
+
+	fn layer_size(layer: i32) -> i32 { 2i32.pow(layer as u32 + 2) }
 
 	fn shift_layer(&mut self, dir: i32) -> bool {
 		let new_layer = self.player_layer + dir;
 		if new_layer < 0 || new_layer >= self.layers.len() as _ { return false }
 
-		self.player_layer += dir;
+		self.player_layer = new_layer;
 		self.player_pos = self.player_pos * Vec2::splat(2.0f32.powi(dir));
 		true
 	}
 
 	fn move_player(&mut self, dir: Vec2i) -> bool {
 		self.player_pos = self.player_pos + dir.to_vec2();
-		let layer_size = 2.0f32.powi(self.player_layer + 2);
+		let layer_size = World::layer_size(self.player_layer) as f32;
 
 		let mut did_warp = false;
 
@@ -372,11 +470,86 @@ impl World {
 		self.layers[self.player_layer as usize].get_tile(pos)
 	}
 
-	fn wrap_position(&self, pos: Vec2i) -> Vec2i {
-		let layer = &self.layers[self.player_layer as usize];
+	fn get_tile_below(&self, pos: Vec2i) -> Option<u8> {
+		if self.player_layer <= 0 {
+			return None
+		}
 
-		Vec2i::new(
-			(pos.x%layer.size.x + layer.size.x) % layer.size.x,
-			(pos.y%layer.size.y + layer.size.y) % layer.size.y)
+		let pos = self.wrap_position(pos);
+		let pos = Vec2i::new(pos.x/2, pos.y/2);
+
+		let next_layer = (self.player_layer-1) as usize;
+		let layer = &self.layers[next_layer];
+
+		layer.get_tile(pos)
+	}
+
+	fn get_tiles_above(&self, pos: Vec2i) -> Option<[u8; 4]> {
+		if self.player_layer + 1 >= self.layers.len() as _ {
+			return None
+		}
+
+		let pos = self.wrap_position(pos);
+		let pos = Vec2i::new(pos.x*2, pos.y*2);
+
+		let layer = &self.layers[(self.player_layer+1) as usize];
+
+		let mut tiles = [0u8; 4];
+		let poss = [
+			pos + Vec2i::new(0, 0),
+			pos + Vec2i::new(0, 1),
+			pos + Vec2i::new(1, 1),
+			pos + Vec2i::new(1, 0),
+		];
+
+		for (p, t) in poss.iter().zip(tiles.iter_mut()) {
+			*t = layer.get_tile(*p)?;
+		}
+
+		Some(tiles)
+	}
+
+	fn get_tile_info(&self, pos: Vec2i) -> Option<&TileInfo> {
+		self.get_tile(pos).iter()
+			.filter_map(|&idx| self.tile_set.get_tile_info(idx as usize))
+			.next()
+	}
+	
+	fn wrap_position(&self, pos: Vec2i) -> Vec2i {
+		self.layers[self.player_layer as usize].wrap_position(pos)
+	}
+}
+
+
+trait MeshBuilderTileExtension {
+	fn draw_tiled_rotated(&mut self, texel_offset: Vec2i, texel_size: Vec2i, pos: Vec2, rot: u32);
+	fn draw_tiled(&mut self, texel_offset: Vec2i, texel_size: Vec2i, pos: Vec2);
+}
+
+impl MeshBuilderTileExtension for MeshBuilder<Vert2D> {
+	fn draw_tiled_rotated(&mut self, texel_offset: Vec2i, texel_size: Vec2i, pos: Vec2, rot: u32) {
+		let base_uv = texel_offset.to_vec2() * TEXEL_FACTOR;
+		let uv_size = texel_size.to_vec2() * TEXEL_FACTOR;
+		let size = texel_size.to_vec2() / Vec2::splat(TEXELS_PER_TILE as f32);
+
+		let rot = rot as usize;
+
+		let uvs = [
+			base_uv + Vec2::new(0.01, 0.98) * uv_size,
+			base_uv + Vec2::new(0.01, 0.01) * uv_size,
+			base_uv + Vec2::new(0.98, 0.01) * uv_size,
+			base_uv + Vec2::new(0.98, 0.98) * uv_size,
+		];
+
+		self.add_quad(&[
+			Vert2D(Vec2::new(0.0, 0.0)*size + pos, uvs[(rot+0)%4]),
+			Vert2D(Vec2::new(0.0, 1.0)*size + pos, uvs[(rot+1)%4]),
+			Vert2D(Vec2::new(1.0, 1.0)*size + pos, uvs[(rot+2)%4]),
+			Vert2D(Vec2::new(1.0, 0.0)*size + pos, uvs[(rot+3)%4]),
+		]);
+	}
+
+	fn draw_tiled(&mut self, texel_offset: Vec2i, texel_size: Vec2i, pos: Vec2) {
+		self.draw_tiled_rotated(texel_offset, texel_size, pos, 0);
 	}
 }
