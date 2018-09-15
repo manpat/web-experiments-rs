e8356c5df9a6ff591c3fa11466983e7ce21bc8e2
manpat <pat@patsown.com>
2018-09-15 19:02:36 +0100

    [polyhedron] Clarified stencil ops; moved to quaternions for rotation; Clicking does stuff now

--- src/bin/polyhedron.rs
+++ src/bin/polyhedron.rs
@@ -4,7 +4,7 @@ extern crate experiments;
 use experiments::*;
 use experiments::rendering::*;
 
-use rand::{Rng, thread_rng};
+use rand::{Rng, Rand, thread_rng};
 
 use events::Event;
 
@@ -30,6 +30,7 @@ fn main() {
 			events::initialise_ems_event_queue(&mut events);
 
 			gl::Enable(gl::BLEND);
+			gl::Enable(gl::STENCIL_TEST);
 			gl::BlendEquation(gl::FUNC_ADD);
 			gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
 
@@ -51,6 +52,14 @@ fn main() {
 		let mut screen_size = Vec2i::zero();
 		let mut projection = Mat4::ident();
 
+		let mut inset_rotation = Quat::ident();
+		let mut outer_rotation = Quat::ident();
+		let mut outer2_rotation = Quat::ident();
+
+		let mut inset_rotation_delta = Quat::ident();
+
+		let mut rng = thread_rng();
+
 		loop {
 			for e in events.iter() {
 				match *e {
@@ -63,6 +72,11 @@ fn main() {
 					}
 
 					Event::Down(_) => {
+						let impulse = Quat::new(
+							Vec3::rand(&mut rng).normalize(),
+							rng.gen_range(PI / 2.0, PI * 2.0 / 3.0) / 12.0);
+
+						inset_rotation_delta = inset_rotation_delta * impulse;
 					}
 
 					_ => {}
@@ -71,71 +85,88 @@ fn main() {
 
 			events.clear();
 
-			time += 1.0/60.0;
+			const DT: f32 = 1.0/60.0;
+			time += DT;
+
+			let generate_rotation = |time: f32| {
+				let alpha = 0.0 * time * PI / 4.0 + (time / 5.0).cos() * (time / 2.0).sin() * PI / 3.0;
+				let beta = 0.0 * time * PI / 5.0 + (time / 2.0).cos() * (time / 11.0).sin() * PI / 4.0;
+				let zeta = 0.0 * time * PI / 11.0 + (time / 3.0).cos() * (time / 7.0).cos() * PI / 7.0;
+
+				let result = Quat::new(Vec3::from_z(1.0), zeta)
+				* Quat::new(Vec3::from_x(1.0), beta)
+				* Quat::new(Vec3::from_y(1.0), alpha);
+
+				result.scale(DT * 0.01)
+			};
+
+			inset_rotation = DT.ease_linear(
+				inset_rotation,
+				Quat::new(Vec3::from_y(1.0), PI / 4.0 * time));
+
+			inset_rotation_delta = inset_rotation_delta.scale(1.0 - DT / 2.0) * generate_rotation(time);
+
+			inset_rotation = (inset_rotation * inset_rotation_delta).normalize();
+			outer_rotation = (16.0 * DT).ease_linear(outer_rotation, inset_rotation).normalize();
+			outer2_rotation = (16.0 * DT).ease_linear(outer2_rotation, outer_rotation).normalize();
 
 			webgl.clear_all();
 			webgl.set_viewport(screen_size);
 
-			let generate_rotation = |time: f32| {
-				let alpha = time * PI / 4.0 + (time / 5.0).cos() * (time / 2.0).sin() * PI / 3.0;
-				let beta = time * PI / 5.0 + (time / 2.0).cos() * (time / 11.0).sin() * PI / 4.0;
-				let zeta = time * PI / 11.0 + (time / 3.0).cos() * (time / 7.0).cos() * PI / 7.0;
+			let translation = Mat4::translate(Vec3::from_z(-2.0));
 
-				  Mat4::zrot(zeta)
-				* Mat4::xrot(beta)
-				* Mat4::yrot(alpha)
-			};
+			let outer2_mat = translation * outer2_rotation.to_mat4() * Mat4::scale(Vec3::splat(1.2));
+			let outer_mat = translation * outer_rotation.to_mat4();
 
-			let view_mat = Mat4::translate(Vec3::from_z(-2.0))
-				* generate_rotation(time);
+			let scaled_outer2_mat = outer2_mat * Mat4::scale(Vec3::splat(0.9));
+			let scaled_outer_mat = outer_mat * Mat4::scale(Vec3::splat(0.9));
 
-			let scaled_view_mat = view_mat * Mat4::scale(Vec3::splat(0.9));
+			let inset_view_mat = Mat4::translate(Vec3::from_z(-40.0))
+				* inset_rotation.to_mat4()
+				* Mat4::scale(Vec3::splat(10.0));
 
 			shader.use_program();
 			shader.set_proj(&projection);
 
 			mesh.bind();
 
-			unsafe {
-				gl::Enable(gl::STENCIL_TEST);
 
-				gl::ColorMask(0, 0, 0, 0);
-				gl::DepthMask(0);
-				gl::StencilFunc(gl::ALWAYS, 0x1, 0xff);
-				gl::StencilOp(gl::ZERO, gl::ZERO, gl::REPLACE);
-			}
+			// Prepare stencil buffer
+			webgl.disable_color_write();
+			webgl.disable_depth_write();
 			
-			shader.set_view(&view_mat);
+			webgl.set_stencil(StencilParams::new(0x1).always().replace());
+			shader.set_view(&outer2_mat);
 			mesh.draw(gl::TRIANGLES);
 
-			unsafe {
-				gl::StencilFunc(gl::NOTEQUAL, 0x0, 0xff);
-				gl::StencilOp(gl::KEEP, gl::KEEP, gl::INCR);
-			}
-
-			shader.set_view(&scaled_view_mat);
+			webgl.set_stencil(StencilParams::new(0x1).equal().increment());
+			shader.set_view(&scaled_outer2_mat);
+			mesh.draw(gl::TRIANGLES);
+			
+			webgl.set_stencil(StencilParams::new(0x2).equal().increment());
+			shader.set_view(&outer_mat);
 			mesh.draw(gl::TRIANGLES);
 
-			unsafe {
-				gl::StencilOp(gl::KEEP, gl::KEEP, gl::KEEP);
-				gl::ColorMask(1, 1, 1, 1);
-				gl::DepthMask(1);
-
-				gl::StencilFunc(gl::EQUAL, 0x1, 0xff);
-			}
-			
-			shader.set_view(&view_mat);
+			webgl.set_stencil(StencilParams::new(0x3).equal().increment());
+			shader.set_view(&scaled_outer_mat);
 			mesh.draw(gl::TRIANGLES);
 
-			unsafe {
-				gl::StencilFunc(gl::LESS, 0x1, 0xff);
-			}
 
-			let view_mat = Mat4::translate(Vec3::from_z(-40.0))
-				* generate_rotation(time + 0.3)
-				* Mat4::scale(Vec3::splat(10.0));
+			// Draw outline
+			webgl.enable_color_write();
+			webgl.enable_depth_write();
 			
-			shader.set_view(&view_mat);
+			webgl.set_stencil(StencilParams::new(0x3).equal());
+			shader.set_view(&outer_mat);
+			mesh.draw(gl::TRIANGLES);
+			
+			webgl.set_stencil(StencilParams::new(0x1).equal());
+			shader.set_view(&outer2_mat);
+			mesh.draw(gl::TRIANGLES);
+
+			// Draw inset
+			webgl.set_stencil(StencilParams::new(0x4).equal());
+			shader.set_view(&inset_view_mat);
 			mesh.draw(gl::TRIANGLES);
 
 			yield;
